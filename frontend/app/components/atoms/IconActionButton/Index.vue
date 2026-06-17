cf0a3042978324a762c7c0bc8337d9d04b921077
PedroHRFerreira <devpedrohr@gmail.com>
2026-06-17 18:04:58 -0300

    feat(ui): add icon action button atom

---
+++ frontend/app/components/atoms/IconActionButton/Index.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IconActionButton',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    variant: {
+      type: String,
+      default: 'secondary'
+    }
+  },
+  emits: ['click'],
+  setup(_, { emit }) {
+    function handleClick() {
+      emit('click')
+    }
+
+    return {
+      handleClick
+    }
+  }
+})
+</script>
+
+<template>
+  <button class="button icon-action-button" :class="`button--${variant}`" :title="title" type="button" @click="handleClick">
+    <slot />
+  </button>
+</template>
+
+<style scoped lang="scss">
+@use "styles.module.scss";
+</style>
