dbca043f21014ac07e1876701632b17a5a4c1e0d
PedroHRFerreira <devpedrohr@gmail.com>
2026-06-17 18:04:58 -0300

    feat(ui): add brand mark atom

---
+++ frontend/app/components/atoms/BrandMark/Index.vue
@@ -0,0 +1,47 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BrandMark",
+  props: {
+    initials: {
+      type: String,
+      default: "EA",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      default: "",
+    },
+    to: {
+      type: String,
+      default: "",
+    },
+  },
+});
+</script>
+
+<template>
+  <NuxtLink v-if="to" class="brand-mark" :to="to">
+    <span class="brand-mark__badge">{{ initials }}</span>
+    <span class="brand-mark__content">
+      <strong>{{ title }}</strong>
+      <small v-if="subtitle">{{ subtitle }}</small>
+    </span>
+  </NuxtLink>
+
+  <div v-else class="brand-mark">
+    <span class="brand-mark__badge">{{ initials }}</span>
+    <span class="brand-mark__content">
+      <strong>{{ title }}</strong>
+      <small v-if="subtitle">{{ subtitle }}</small>
+    </span>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "styles.module.scss";
+</style>
