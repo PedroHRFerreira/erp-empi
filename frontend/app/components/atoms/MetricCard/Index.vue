22839138a9dcf6efaa1709211f2dd6d26020aa6b
PedroHRFerreira <devpedrohr@gmail.com>
2026-06-17 18:04:59 -0300

    feat(metrics): add metric card atom

---
+++ frontend/app/components/atoms/MetricCard/Index.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import { defineComponent, type PropType } from "vue";
+
+export default defineComponent({
+  name: "MetricCard",
+  props: {
+    icon: {
+      type: [Object, Function] as PropType<unknown>,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: [String, Number],
+      required: true,
+    },
+  },
+});
+</script>
+
+<template>
+  <article class="metric-card panel">
+    <component :is="icon" :size="22" />
+    <span>{{ label }}</span>
+    <strong>{{ value }}</strong>
+  </article>
+</template>
+
+<style scoped lang="scss">
+@use "styles.module.scss";
+</style>
